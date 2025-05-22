47294e1e82fb80f715c31e32b819ea82187bc3f4
oualidelh <elhouarioualid@gmail.com>
2025-05-22 16:13:09 +0100

    add auth

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -5,8 +5,8 @@ export async function updateSession(request: NextRequest) {
   const supabaseResponse = NextResponse.next({ request });
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL1!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY1!,
     {
       cookies: {
         getAll: () => request.cookies.getAll(),
